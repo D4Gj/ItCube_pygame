247d75d1d08b0aee4463005e9e26a4a511908df3
D4Gj <inzadimonax@gmail.com>
2022-11-14 16:18:24 +0400

    Version 1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,60 +1,36 @@
-import sys
 import pygame
+import sys
 from settings import Settings
 from ship import Ship
 
 
 class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры"""
 
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption("Alien Invasion")
-
-        self.ship = Ship(self)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.ship = Ship(self.screen)
+        pygame.display.set_caption('Alien invasion')
 
     def run_game(self):
         while True:
-            # Отслеживание событий клавиатуры и мыши
             self._check_events()
-            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                self._check_keydown_events(event)
-            elif event.type == pygame.KEYUP:
-                self._check_keyup_events(event)
-
-    def _check_keydown_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        if event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
-        if event.key == pygame.K_q:
-            sys.exit()
-
-    def _check_keyup_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        if event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        # Отображение последнего кадра
         pygame.display.flip()
 
 
 if __name__ == '__main__':
     ai = AlienInvasion()
-    ai2 = AlienInvasion()
-    ai2.run_game()
     ai.run_game()
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-class Settings():
+class Settings:
+    """Класс для хранения настроек"""
 
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
-        self.ship_speed = 15
+        self.bg_color = (255, 255, 111)
