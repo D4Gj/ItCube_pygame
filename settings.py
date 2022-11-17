c2ef3eb8e92be128979f4e17d63ca77b7ea48e04
D4Gj <inzadimonax@gmail.com>
2022-11-17 16:04:10 +0400

    Добавлено передвижение корабля и отработка коллизий

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (255, 255, 111)
+        self.ship_speed = 10.5
--- ship.py
+++ ship.py
@@ -2,14 +2,25 @@ import pygame
 
 
 class Ship:
-    def __init__(self, screen):
-        self.screen = screen
-        self.screen_rect = screen.get_rect()
-
+    def __init__(self, game):
+        self.screen = game.screen
+        self.screen_rect = game.screen.get_rect()
+        self.settings = game.settings
         self.image = pygame.image.load("images/ship.png")
         self.rect = self.image.get_rect()
-
+        self.x = float(self.rect.x)
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
