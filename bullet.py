6efb9b22af73b83f98584d4ece9fa96fb4758325
D4Gj <inzadimonax@gmail.com>
2022-11-28 16:15:27 +0400

    Домашнее задание выполненное

--- bullet.py
+++ bullet.py
@@ -6,19 +6,30 @@ class Bullet(Sprite):
 
     def __init__(self, game):
         super().__init__()
+        self.game = game
         self.screen = game.screen
         self.settings = game.settings
         self.color = self.settings.bullet_color
 
-        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
-                                self.settings.bullet_height)
+        self.image = pygame.image.load('images/bullet.png')
+        # self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+        #                        self.settings.bullet_height)
+        self.rect = self.image.get_rect()
         self.rect.midtop = game.ship.rect.midtop
 
         self.y = float(self.rect.y)
+        self.x = float(self.rect.x)
 
     def update(self):
         self.y -= self.settings.bullet_speed
+        if self.game.ship.moving_left:
+            self.x -= self.settings.bullet_speed
+        elif self.game.ship.moving_right:
+            self.x += self.settings.bullet_speed
+
         self.rect.y = self.y
+        self.rect.x = self.x
 
     def draw_bullet(self):
-        pygame.draw.rect(self.screen, self.color, self.rect)
+        # pygame.draw.rect(self.screen, self.color, self.rect)
+        self.screen.blit(self.image, self.rect)
--- ship.py
+++ ship.py
@@ -9,18 +9,25 @@ class Ship:
         self.image = pygame.image.load("images/ship.png")
         self.rect = self.image.get_rect()
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         self.rect.midbottom = self.screen_rect.midbottom
 
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left > 0:
+        elif self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-
+        elif self.moving_up and self.rect.y > 0:
+            self.y -= self.settings.ship_speed
+        elif self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
